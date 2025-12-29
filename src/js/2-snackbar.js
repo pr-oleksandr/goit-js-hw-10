1f79372e056037a5ca31bd02116080aca3406f9a
Саша Присташ <sashko@MacBook-Air-Sasa.local>
2025-12-29 21:59:10 +0200

    update snackbar.js

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,42 +1,42 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-const getForm = document.querySelector('.form');
-const getDelay = getForm.querySelector('input[name = "delay"]');
-getForm.addEventListener('submit', handlerSubmit);
+const form = document.querySelector('.form');
+form.addEventListener('submit', handlerSubmit);
 
 function handlerSubmit(event) {
   event.preventDefault();
-  const getFulfilledValue = getForm.querySelector(
-    'input[name="state"]:checked'
-  );
-  const delay = Number(getDelay.value);
-  const createMessage = setTimeout(() => {
-    const prom = new Promise((resolve, reject) => {
-      if (getFulfilledValue.value === 'fulfilled') {
-        resolve('Fulfilled');
-      } else {
-        reject('Rejected');
-      }
-    });
 
-    prom
-      .then(value =>
-        iziToast.show({
-          message: `${value} promise in ${delay}ms`,
-          messageColor: ' #fff',
-          backgroundColor: '#008000',
-          position: 'topRight',
-        })
-      )
+  const delay = Number(form.elements.delay.value);
+  const state = form.elements.state.value;
 
-      .catch(value => {
-        iziToast.show({
-          message: `${value} promise in ${delay}ms`,
-          messageColor: ' #fff',
-          backgroundColor: '#ef4040',
-          position: 'topRight',
-        });
+  createPromise(delay, state)
+    .then(value =>
+      iziToast.show({
+        message: `Fulfilled promise in ${delay}ms`,
+        messageColor: ' #fff',
+        backgroundColor: '#008000',
+        position: 'topRight',
+      })
+    )
+    .catch(value => {
+      iziToast.show({
+        message: `Rejected promise in ${delay}ms`,
+        messageColor: ' #fff',
+        backgroundColor: '#ef4040',
+        position: 'topRight',
       });
-  }, delay);
+    });
+}
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(state);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
 }
